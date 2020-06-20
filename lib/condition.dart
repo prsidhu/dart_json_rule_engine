22879bca461a965355340304fd4442e610908cbb
prsidhu <preetsidhu.bits@gmail.com>
2020-06-20 14:55:13 +0530

    criteria parser

--- lib/condition.dart
+++ lib/condition.dart
@@ -9,21 +9,54 @@ import 'package:flutter/foundation.dart';
 const errorPrefix = 'Faile to parse';
 
 class Condition {
-  Rule rule;
+  dynamic rules;
   Event event;
 
-  Condition({
-    @required this.rule,
-    @required this.event
-  });
+  Condition({@required this.rules, @required this.event});
 
   factory Condition.fromJson(Map<String, dynamic> map) {
-    if(!validateKey('rule', map) || !validateKey('event', map)) throw Error.safeToString('$errorPrefix condition');
-    return Condition(
-      event: Event.fromJson(map['event']),
-      rule: Rule.fromJson(map['rule'])
-    );
+    try {
+      if (!validateKey('rule', map) || !validateKey('event', map))
+        throw Error.safeToString('$errorPrefix condition');
+      return Condition(
+          event: Event.fromJson(map['event']),
+          rules: Condition.tryParse(map['rule']);
+    } catch (e) {
+      print(e);
+      throw Error.safeToString('$errorPrefix condition');
+    }
   }
 
-  factory Condition.fromRawJson(String str) => Condition.fromJson(jsonDecode(str));
+  factory Condition.fromRawJson(String str) =>
+      Condition.fromJson(jsonDecode(str));
+
+  static dynamic tryParse(dynamic obj, [bool flag = true]) {
+    try {
+      if (obj != null) {
+        if (obj is Map) {
+          if (!obj.containsKey('all') && !obj.containsKey('any')) {
+            Rule rule = Rule.tryParse(obj);
+            if (rule == null) throw Error();
+            return flag ? {'all': List()..add(rule)} : rule;
+          } else {
+            Map res = {};
+            List keys = obj.keys.toList();
+            keys.forEach((key) {
+              res[key] = tryParse(obj[key], false);
+            });
+            return res;
+          }
+        } else if (obj is List) {
+          List list = [];
+          for (var item in obj) {
+            list.add(tryParse(item, false));
+          }
+          return list;
+        }
+      } throw Error();
+    } catch (e) {
+      print(e);
+      throw Error();
+    }
+  }
 }
